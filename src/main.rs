a7958f242b16b3b53a2b7f4758e82f8b921238b4
Mika Viinamäki <kauhsa@kapsi.fi>
2022-10-06 19:07:30 +0300

    Ctrl+C exits gracefully

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,20 @@ mod event;
 mod event_generator;
 mod player;
 
-use crossbeam_channel::unbounded;
+use crossbeam_channel::{unbounded, RecvTimeoutError};
 use event::Event;
 use event_generator::EventGenerator;
+use log::debug;
 use midir::os::unix::VirtualOutput;
 use midir::MidiOutput;
 use player::Player;
-use std::thread::{self, sleep};
+use std::thread;
 use std::time::Duration;
 use thread_priority::*;
+
+pub enum UiEvent {
+    Exit,
+}
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
@@ -19,37 +24,70 @@ fn main() -> anyhow::Result<()> {
 
     let (event_sender, event_receiver) = unbounded::<Event>();
 
+    /* event generator thread */
+
+    let (ui_sender_for_event, ui_receiver_for_event) = unbounded::<UiEvent>();
     let event_generator_thread = thread::Builder::new()
         .name("event_generator".to_string())
         .spawn(move || {
-            let mut exec = EventGenerator::create(event_sender).unwrap();
+            debug!("Event generator thread started");
+
+            let mut event_generator =
+                EventGenerator::create(event_sender).expect("Could not create event generator");
 
             // temporary hack - request 1500ms worth of events every 1000ms, so
             // we should not ever run out
             loop {
-                exec.request_notes(1500).unwrap();
-                sleep(Duration::from_millis(1000))
+                event_generator.request_notes(1500).unwrap();
+
+                match ui_receiver_for_event.recv_timeout(Duration::from_millis(1000)) {
+                    Ok(UiEvent::Exit) => break,
+                    Err(RecvTimeoutError::Timeout) => (),
+                    Err(e) => panic!("{}", e),
+                }
             }
+
+            debug!("Event generator thread exited");
         })?;
 
-    // tried only for OSX
-    let player_thread_priority = ThreadPriority::Crossplatform(40.try_into().unwrap());
+    /* player thread */
 
+    let player_thread_priority = ThreadPriority::Crossplatform(40.try_into().unwrap());
+    let (ui_sender_for_player, ui_receiver_for_player) = unbounded::<UiEvent>();
     let player_thread = ThreadBuilder::default()
         .name("player".to_string())
         .priority(player_thread_priority)
         .spawn_careless(move || {
+            debug!("Player thread started");
+
             let midi_output_connection = midi_out
                 .create_virtual("Virtual port")
                 .expect("Could not create midi output connection");
 
-            let mut player = Player {
-                receiver: event_receiver,
+            let mut player = Player::new(
+                event_receiver.clone(),
                 midi_output_connection,
-            };
+                ui_receiver_for_player,
+            );
 
-            player.play().unwrap()
+            player.play().unwrap();
+
+            debug!("Player thread exited");
         })?;
+
+    ctrlc::set_handler(move || {
+        // TODO: separate channels probably won't scale.
+
+        ui_sender_for_event
+            .send(UiEvent::Exit)
+            .expect("Could not send exit signal");
+
+        ui_sender_for_player
+            .send(UiEvent::Exit)
+            .expect("Could not send exit signal");
+    })?;
+
+    /* let's go! */
 
     event_generator_thread.join().unwrap();
     player_thread.join().unwrap();
--- src/player.rs
+++ src/player.rs
@@ -1,47 +1,83 @@
 use std::time::{Duration, Instant};
 
-use crossbeam_channel::Receiver;
+use crossbeam_channel::{select, Receiver};
 use log::{debug, info};
 use midir::MidiOutputConnection;
 
-use crate::event::Event;
+use crate::{event::Event, UiEvent};
 
 pub struct Player {
     pub receiver: Receiver<Event>,
     pub midi_output_connection: MidiOutputConnection,
+    pub ui_receiver: Receiver<UiEvent>,
+
+    // internal player state
+    first_event_instant: Option<Instant>,
+    should_have_elapsed: Duration,
 }
 
 impl Player {
+    pub fn new(
+        event_receiver: Receiver<Event>,
+        midi_output_connection: MidiOutputConnection,
+        ui_receiver: Receiver<UiEvent>,
+    ) -> Self {
+        Player {
+            receiver: event_receiver,
+            ui_receiver,
+            midi_output_connection,
+
+            first_event_instant: None,
+            should_have_elapsed: Duration::ZERO,
+        }
+    }
+
     pub fn play(&mut self) -> anyhow::Result<()> {
-        let mut start: Option<Instant> = None;
-        let mut should_have_elapsed = Duration::ZERO;
-
         loop {
-            let event = self.receiver.recv().expect("Channel died");
+            // TODO: ui_receiver should have a priority, which is does not right
+            // now.
+            select! {
+                recv(self.ui_receiver) -> e => match e {
+                    Ok(UiEvent::Exit) => break,
+                    _ => ()
+                },
 
-            if start.is_none() {
-                start = Some(Instant::now())
-            }
-
-            debug!("Event: {:?}", event);
-
-            match &event {
-                Event::NoteOn(e) => self.send_to_midi(&e.to_midi_msg())?,
-
-                Event::NoteOff(e) => self.send_to_midi(&e.to_midi_msg())?,
-
-                Event::Print { value } => {
-                    info!("Print: {}", value)
-                }
-
-                Event::Break(e) => {
-                    should_have_elapsed += Duration::from_secs_f32(e.duration / 1000.0);
-                    let sleep_duration = should_have_elapsed - start.unwrap().elapsed();
-                    debug!("Break, sleeping {:?}", sleep_duration);
-                    spin_sleep::sleep(sleep_duration)
+                recv(self.receiver) -> e => match e {
+                    Ok(event) => self.process_new_event(event)?,
+                    _ => ()
                 }
             }
         }
+
+        Ok(())
+    }
+
+    fn process_new_event(&mut self, event: Event) -> anyhow::Result<()> {
+        if self.first_event_instant.is_none() {
+            self.first_event_instant = Some(Instant::now())
+        }
+
+        debug!("Event: {:?}", event);
+
+        match &event {
+            Event::NoteOn(e) => self.send_to_midi(&e.to_midi_msg())?,
+
+            Event::NoteOff(e) => self.send_to_midi(&e.to_midi_msg())?,
+
+            Event::Print { value } => {
+                info!("Print: {}", value)
+            }
+
+            Event::Break(e) => {
+                self.should_have_elapsed += Duration::from_secs_f32(e.duration / 1000.0);
+                let sleep_duration =
+                    self.should_have_elapsed - self.first_event_instant.unwrap().elapsed();
+                debug!("Break, sleeping {:?}", sleep_duration);
+                spin_sleep::sleep(sleep_duration)
+            }
+        }
+
+        Ok(())
     }
 
     fn send_to_midi(&mut self, msg: &[u8]) -> anyhow::Result<()> {
@@ -55,6 +91,8 @@ const ALL_NOTES_OFF: [u8; 3] = [0xB0, 0, 0];
 
 impl Drop for Player {
     fn drop(&mut self) {
+        debug!("Sending all notes off signal");
+
         self.midi_output_connection
             .send(&ALL_NOTES_OFF)
             .expect("Could not send all notes out message");
