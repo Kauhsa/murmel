e2054af49f9aaccd727ac4e667db94d54bffe020
Mika Viinamäki <kauhsa@kapsi.fi>
2022-10-06 14:33:16 +0300

    Rename types

--- src/event.rs
+++ src/event.rs
@@ -1,15 +1,13 @@
 use deno_core::serde::Deserialize;
 
-// beats? dunno.
-pub type Duration = u32;
-
-// notes
-pub type Note = u8;
+/** beats? milliseconds? dunno yet. */
+pub type NoteDuration = u32;
+pub type NoteValue = u8;
 
 #[derive(Deserialize, Debug)]
 #[serde(tag = "type")]
 pub enum Event {
-    NoteOn { note: Note },
-    NoteOff { note: Note },
-    Break { duration: Duration },
+    NoteOn { note: NoteValue },
+    NoteOff { note: NoteValue },
+    Break { duration: NoteDuration },
 }
